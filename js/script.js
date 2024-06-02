cfc2c5a3d434e3e427e3c388342ffc24fa14a68f
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-06-02 09:00:53 +0200

    Rendering statistics of tasks

--- js/script.js
+++ js/script.js
@@ -3,68 +3,6 @@
 
     let hideDoneTasks = false;
 
-    const removeTask = removeTaskIndex => {
-        tasks = [
-            ...tasks.slice(0, removeTaskIndex),
-            ...tasks.slice(removeTaskIndex + 1),
-        ];
-        render();
-    };
-
-    const toggleDone = toggleDoneIndex => {
-        tasks = tasks.map((task, index) =>
-            index === toggleDoneIndex ?
-                { ...task, done: !task.done } :
-                task
-        );
-        render();
-    };
-
-    const callToggleDoneEvents = () => {
-        const toggleDoneStatusButton = document.querySelectorAll(".js-doneButton");
-        toggleDoneStatusButton.forEach((toggleDoneButton, toggleDoneIndex) => {
-            toggleDoneButton.addEventListener("click", () => {
-                toggleDone(toggleDoneIndex);
-            });
-        });
-    };
-
-    const callRemoveTaskEvent = () => {
-        const removeTaskButton = document.querySelectorAll(".js-removeTaskButton");
-        removeTaskButton.forEach((removeButton, removeTaskIndex) => {
-            removeButton.addEventListener("click", () => {
-                removeTask(removeTaskIndex);
-            });
-        });
-    };
-
-    const renderTasks = () => {
-        let htmlStringOfTaskListItem = "";
-        for (const task of tasks) {
-            htmlStringOfTaskListItem += ` 
-            <li class="js-taskListItem taskList__listItem ${hideDoneTasks && task.done ? 'taskList__listItem--hidden' : ''}"> 
-                <button class="tasksList__button tasksList__button--done js-doneButton">${task.done ? "✔" : ""}</button>
-                <p class="taskList__taskContent ${task.done ? 'taskList__taskContent--done' : ''}">${task.content}</p>
-                <button class="tasksList__button js-removeTaskButton">X</button>
-            </li>
-            `};
-        document.querySelector(".js-tasksList").innerHTML = htmlStringOfTaskListItem;
-    };
-
-    const renderButtons = () => {
-        let managmentButtonsHtmlString = `
-        <button class=" js-toggleDonedTasksVisibility
-        ${tasks.length > 0 ? 'taskList__managementTasksButton--visibly' : 'taskList__managementTasksButton'}">
-            ${hideDoneTasks ? 'Pokaż' : 'Ukryj'} ukończone
-        </button>
-        <button ${tasks.every(({ done }) => done) ? 'disabled' : ''} class="js-doneAllTasksButton
-        ${tasks.length > 0 ? 'taskList__managementTasksButton--visibly' : 'taskList__managementTasksButton'}">
-            Ukończ wszystkie
-        </button>
-        `;
-        document.querySelector(".js-tasksListButtonContainer").innerHTML = managmentButtonsHtmlString;
-    };
-
     const toggleVisibilityOfDonedTasks = () => {
         hideDoneTasks = !hideDoneTasks;
         if (hideDoneTasks && tasks.every(({ done }) => !done)) {
@@ -92,9 +30,84 @@
         };
     };
 
+    const toggleDone = toggleDoneIndex => {
+        tasks = tasks.map((task, index) =>
+            index === toggleDoneIndex ?
+                { ...task, done: !task.done } :
+                task
+        );
+        render();
+    };
+
+    const callToggleDoneEvents = () => {
+        const toggleDoneStatusButton = document.querySelectorAll(".js-doneButton");
+        toggleDoneStatusButton.forEach((toggleDoneButton, toggleDoneIndex) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleDone(toggleDoneIndex);
+            });
+        });
+    };
+
+    const removeTask = removeTaskIndex => {
+        tasks = [
+            ...tasks.slice(0, removeTaskIndex),
+            ...tasks.slice(removeTaskIndex + 1),
+        ];
+        render();
+    };
+
+    const callRemoveTaskEvent = () => {
+        const removeTaskButton = document.querySelectorAll(".js-removeTaskButton");
+        removeTaskButton.forEach((removeButton, removeTaskIndex) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(removeTaskIndex);
+            });
+        });
+    };
+
+    const renderStats = () => {
+        const htmlStringOfStats = `
+        <p class="tasksList__statsItem ${tasks.length > 0 ? '' : 'tasksList__statsItem--hidden'}"> 
+            Liczba wszystkich zadań: ${tasks.length}</p>
+        <p class="tasksList__statsItem tasksList__statsItem--finishedTasks ${tasks.length > 0 ? '' : 'tasksList__statsItem--hidden'}">
+            Liczba ukończonych zadań: 
+            ${tasks.filter(({ done }) => done).length}
+        </p>
+        `;
+        document.querySelector(".js-statsContainer").innerHTML = htmlStringOfStats;
+    };
+
+    const renderButtons = () => {
+        const managmentButtonsHtmlString = `
+        <button class=" js-toggleDonedTasksVisibility
+        ${tasks.length > 0 ? 'taskList__managementTasksButton--visibly' : 'taskList__managementTasksButton'}">
+            ${hideDoneTasks ? 'Pokaż' : 'Ukryj'} ukończone
+        </button>
+        <button ${tasks.every(({ done }) => done) ? 'disabled' : ''} class="js-doneAllTasksButton
+        ${tasks.length > 0 ? 'taskList__managementTasksButton--visibly' : 'taskList__managementTasksButton'}">
+            Ukończ wszystkie
+        </button>
+        `;
+        document.querySelector(".js-tasksListButtonContainer").innerHTML = managmentButtonsHtmlString;
+    };
+
+    const renderTasks = () => {
+        let htmlStringOfTaskListItem = "";
+        for (const task of tasks) {
+            htmlStringOfTaskListItem += ` 
+            <li class="js-taskListItem taskList__listItem ${hideDoneTasks && task.done ? 'taskList__listItem--hidden' : ''}"> 
+                <button class="tasksList__button tasksList__button--done js-doneButton">${task.done ? "✔" : ""}</button>
+                <p class="taskList__taskContent ${task.done ? 'taskList__taskContent--done' : ''}">${task.content}</p>
+                <button class="tasksList__button js-removeTaskButton">X</button>
+            </li>
+            `};
+        document.querySelector(".js-tasksList").innerHTML = htmlStringOfTaskListItem;
+    };
+
     const render = () => {
         renderTasks();
         renderButtons();
+        renderStats();
 
         callRemoveTaskEvent();
         callToggleDoneEvents();
